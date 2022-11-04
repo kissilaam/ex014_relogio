6892fa3ecce33db92f00c78a15e414b6136a6474
kissilaam <kissila.am@gmail.com>
2022-11-04 16:17:59 -0300

    atualizado readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ex014_relogio
 
 Exercício 14 do Curso de JavaScript do Curso em Vídeo.
-Criado relógio mostrando o horário do sistema local, e imagem correspondente.
+
+Criado relógio mostrando o horário do sistema local, com imagem correspondente que muda a depender do período.
+
+O que aprendi:
+
+- Tratameto de elementos DOM
+- CSS básico
+- JavaScript Conditionals
--- relógio.js
+++ relógio.js
@@ -1,21 +1,21 @@
 function carregar() {
-  var msg = window.document.getElementById('msg')
-  var img = window.document.getElementById('imagem')
-  var data = new Date()
-  var hora = data.getHours()
-  var minutos = data.getMinutes()
-  msg.innerHTML = `Agora são <b>${hora}:${minutos}</b> horas.`
-  if (hora >= 0 && hora < 12) {
-    img.src = 'fotomanha.png'
-    document.body.style.background = '#f1e0c8'
-    msg.innerHTML += `</br>Tenha um ótimo dia!`
-  } else if (hora >= 12 && hora <= 18) {
-    img.src = 'fototarde.png'
-    document.body.style.background = '#dcb077'
-    msg.innerHTML += `</br>Tenha uma ótima tarde!`
-  } else {
-    img.src = 'fotonoite.png'
-    document.body.style.background = '#192e2f'
-    msg.innerHTML += `</br>Tenha uma ótima noite!`
-  }
+	var msg = window.document.getElementById('msg')
+	var img = window.document.getElementById('imagem')
+	var data = new Date()
+	var hora = data.getHours()
+	var minutos = data.getMinutes()
+	msg.innerHTML = `Agora são <b>${hora}:${minutos}</b> horas.`
+	if (hora >= 0 && hora < 12) {
+		img.src = 'fotomanha.png'
+		document.body.style.background = '#f1e0c8'
+		msg.innerHTML += `</br>Tenha um ótimo dia!`
+	} else if (hora >= 12 && hora <= 18) {
+		img.src = 'fototarde.png'
+		document.body.style.background = '#dcb077'
+		msg.innerHTML += `</br>Tenha uma ótima tarde!`
+	} else {
+		img.src = 'fotonoite.png'
+		document.body.style.background = '#192e2f'
+		msg.innerHTML += `</br>Tenha uma ótima noite!`
+	}
 }
